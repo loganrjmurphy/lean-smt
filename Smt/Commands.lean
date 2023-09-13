8673dee9a6a57780465739cec13e95314a6c51b8
loganrjmurphy <loganrjmurphy@gmail.com>
2023-09-13 11:02:50 -0400

    add support for declaring uninterpreted functions

--- Smt/Commands.lean
+++ Smt/Commands.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2021-2022 by the authors listed in the file AUTHORS and their
 institutional affiliations. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Abdalrhman Mohamed, Tomaz Gomes Mascarenhas, Wojciech Nawrocki
+Authors: Abdalrhman Mohamed, Tomaz Gomes Mascarenhas, Wojciech Nawrocki, Logan Murphy
 -/
 
 import Smt.Data.Sexp
@@ -19,6 +19,7 @@ inductive Command where
   | declareSort (nm : String) (arity : Nat)
   | defineSort (nm : String) (ps : List Term) (tm : Term)
   | declare (nm : String) (st : Term)
+  | declareFun (nm : String) (ps : List Term) (cod : Term)
   | defineFun (nm : String) (ps : List (String × Term)) (cod : Term) (tm : Term) (rec : Bool)
   | assert (tm : Term)
   | checkSat
@@ -52,6 +53,8 @@ protected def toSexp : Command → Sexp
     sexp!{(declare-sort {quoteSymbol nm} {toString arity})}
   | .defineSort nm ps tm          =>
     sexp!{(define-sort {quoteSymbol nm} (...{ps.map toSexp}) {tm})}
+  | .declareFun nm ps cod => 
+    sexp!{(declare-fun {quoteSymbol nm} (...{ps.map toSexp}) {cod})}
   | .defineFun nm ps cod tm false =>
     sexp!{(define-fun {quoteSymbol nm} {paramsToSexp ps} {cod} {tm})}
   | .defineFun nm ps cod tm true  =>
